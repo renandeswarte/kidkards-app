b8a8f880346f8672af3edc550b32bbd4962b15ba
Renan Deswarte <renandeswarte@gmail.com>
2016-02-23 20:35:12 -0800

    feat: add fastclick lib for mobile app

--- mobileApp/www/js/index.js
+++ mobileApp/www/js/index.js
@@ -33,19 +33,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        FastClick.attach(document.body);
         // app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    // receivedEvent: function(id) {
-    //     var parentElement = document.getElementById(id);
-    //     var listeningElement = parentElement.querySelector('.listening');
-    //     var receivedElement = parentElement.querySelector('.received');
-
-    //     listeningElement.setAttribute('style', 'display:none;');
-    //     receivedElement.setAttribute('style', 'display:block;');
-
-    //     console.log('Received Event: ' + id);
-    // }
+    }
 };
 
 app.initialize();
